aaabb78cb37359bc577b8c3c9211f2542d279e27
Vsevolod Pletnev <mr.sevapp@gmail.com>
2024-01-01 19:34:28 +0600

    rework ui

--- game/src/ui/App.tsx
+++ game/src/ui/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import "./App.css";
+// import "./App.css";
 // import { initExampleGame } from "../game/example";
 
 function App() {
@@ -11,7 +11,6 @@ function App() {
 
   return (
     <>
-      <div id="game-container" className="border-2 border-primary"></div>
     </>
   );
 }
